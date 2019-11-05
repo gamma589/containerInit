c9c06e600deefddb07439175f20ac499ef92c2eb
alex <gamma589@gmail.com>
2019-11-05 19:57:48 +0200

    need to check if variable in array for bin/sh

--- startup.sh
+++ startup.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
-ALLOWED_CONFIG_TYPE="env file"
+ALLOWED_CONFIG_TYPE="env file none"
 if [ -z "$CONFIG_TYPE" ]
 then
     echo "CONFIG_TYPE is empty, entering infinite loop to allow debug"
     while true; do sleep 86400; done
 else
-    if [[ " ${ALLOWED_CONFIG_TYPE[@]} " ~= " ${CONFIG_TYPE} " ]]
-    then
-        echo "valid CONFIG_TYPE variable"
-    fi
+    # if [[ " ${ALLOWED_CONFIG_TYPE[@]} " ~= " ${CONFIG_TYPE} " ]]
+    # then
+    #     echo "valid CONFIG_TYPE variable"
+    # fi
     echo "CONFIG_TYPE is NOT empty"
 fi
 
@@ -23,9 +23,11 @@ if [ $CONFIG_TYPE == "env" ]
 then
     echo "Populating environment using variables in $CONFIG_PATH ..."
     export $(grep -v '^#' $CONFIG_PATH | xargs)
-else
+elif [ $CONFIG_TYPE == "file" ]
     echo "Writing environment variables to $CONFIG_PATH ..."
     env > $CONFIG_TYPE
+else
+    echo "Container needs no variables or they are handled by some other means"
 fi
 
 
