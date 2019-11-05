df0f4e57623dc2e42e6714c0797447cf1e3b4dc0
alex <gamma589@gmail.com>
2019-11-05 19:43:46 +0200

    Fixed some bash errors

--- startup.sh
+++ startup.sh
@@ -2,19 +2,33 @@
 ALLOWED_CONFIG_TYPE="env file"
 if [ -z "$CONFIG_TYPE" ]
 then
-    echo "\$CONFIG_TYPE is empty, entering infinite loop to allow debug"
-    while true; do sleep 86400
+    echo "CONFIG_TYPE is empty, entering infinite loop to allow debug"
+    while true; do sleep 86400; done
 else
     if [[ " ${ALLOWED_CONFIG_TYPE[@]} " ~= " ${CONFIG_TYPE} " ]]
     then
         echo "valid CONFIG_TYPE variable"
     fi
-    echo "\$CONFIG_TYPE is NOT empty"
+    echo "CONFIG_TYPE is NOT empty"
 fi
 
 if [ -f "$CONFIG_PATH" ]; then
-    echo "$CONFIG_PATH exist"
+    echo "$CONFIG_PATH exists"
+else
+    echo "CONFIG_PATH is empty or invalid, entering infinite loop to allow debug"
+    while true; do sleep 86400; done
 fi
+
+if [ $CONFIG_TYPE == "env" ]
+then
+    echo "Populating environment using variables in $CONFIG_PATH ..."
+    export $(grep -v '^#' $CONFIG_PATH | xargs)
+else
+    echo "Writing environment variables to $CONFIG_PATH ..."
+    env > $CONFIG_TYPE
+fi
+
+
 env
 cd /app
 npm run start:dev --port $PORT
